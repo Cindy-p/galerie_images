74379e8711f2064bb5e6ed963476eb91d2b66e22
Cindy Perat <cindy.perat@live.fr>
2013-11-22 10:24:55 +0100

    Début design

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>NicodyGalerie - Accueil</title>
+		<?php include("include/header.php"); ?>
+	</head>
+	<body>
+		<div id="header">
+			<div id="websiteName">
+				<a href="index.php">NicodyGalerie</a>
+			</div>
+			<div id="navigation">
+				<ul>
+					<li>Accueil</li>
+					<li>Galerie</li>
+					<li>Administration</li>
+				</ul>
+			</div>
+		</div>
+	</body>
+</html>
