3b386cd7e185d18db5f30582c46856e17461a448
VARNIER Nicolas <nicolablonde@hotmail.fr>
2013-12-09 10:25:36 +0100

    header footer

--- include/header.php
+++ include/header.php
@@ -1,4 +1,15 @@
-<link rel="stylesheet" type="text/css" href="css/style.css"/>
-<!--[if lte IE 7]>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8"/>
+		<title><?php echo $page["titre"]; ?></title>
+		<link rel="stylesheet" href="css/style.css"/>
+		<!--[if lte IE 7]>
 <link rel="stylesheet" type="text/css" href="css/style_ie.css"/>
 <![endif]-->
+		
+		<script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+		<script type="text/javascript" src="js/generer_galerie.jquery.js"></script>
+		<script type="text/javascript" src="js/galerie.js"></script>
+	</head>
+	<body>
--- index.php
+++ index.php
@@ -2,21 +2,9 @@
 	if(session_id() == "") session_start(); // Vérification de l'existance de session
 	if (!isset($_SESSION["utilisateur"])) header("Location: authentification.php"); // Redirection si l'utilisateur ne s'est pas identifié
 	header('Content-Type: text/html; charset=utf-8');	// Encodage UTF-8 PHP
-
+		$page = array("titre" => "Nicody Galerie - Accueil");
+		include("include/header.php");
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-	<head>
-		<meta charset="UTF-8"/>
-		<title>Nicody Galerie - Accueil</title>
-		<?php include("include/header.php"); ?>
-		<link rel="stylesheet" href="css/style.css"/>
-		
-		<script type="text/javascript" src="js/jquery-1.9.1.js"></script>
-		<script type="text/javascript" src="js/generer_galerie.jquery.js"></script>
-		<script type="text/javascript" src="js/galerie.js"></script>
-	</head>
-	<body>
 		<div id="entete">
 			<div id="nomSite">
 				<a href="index.php">Nicody Galerie</a>
@@ -92,5 +80,7 @@
 				</div>				
 			</div>
 		</div>
-	</body>
-</html>
+<?php
+
+    include ("include/footer.php");
+?>
