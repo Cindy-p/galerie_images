a2654ce9bac44f3a55b79f4eb2bad165f1305cca
Cindy Perat <cindy.perat@live.fr>
2013-12-10 12:01:28 +0100

    popup affichage image

--- js/generer_galerie.jquery.js
+++ js/generer_galerie.jquery.js
@@ -2,7 +2,7 @@
 	$.fn.genererGalerie = function(parametres) {
 		
 		return this.each(function() {
-			var divConteneurImage = $("img").parent();
+			var divConteneurImage = $("#galerie img").parent();
 			divConteneurImage.addClass('conteneurImage');
 
 			$("#galerie>div").css("display", "inline");
@@ -63,6 +63,73 @@
 					description.css("top", event.pageY);
 					description.show();
 				});
+			});
+
+			// Affichage de l'image en grande taille
+			$("img").click(function() {
+				if ($(this).parent().get(0) == $(".conteneurImage").get(0)) {
+					var imgPrec = "";
+				}
+				else {
+					var imgPrec = $(this).parent().prev().find("img");
+				}
+
+				if () {
+
+				}
+				else {
+					
+				}
+
+				
+				var imgSuiv = $(this).parent().next().find("img");
+
+				// console.log(imgPrec);
+
+				$("#corps").append('<div id="popup"><img src="' + $(this).attr("src") + '"/></div>');
+				
+				console.log(imgPrec);
+				console.log(imgSuiv);
+
+				if (imgPrec != "") {
+					$("#popup").append('<span id="imgPrec"> < </span>');
+				}
+
+				if (imgSuiv != null) {
+					$("#popup").append('<span id="imgSuiv"> > </span>');
+				}
+				
+				
+
+				$("body").append('<div id="fade"></div>');
+
+				// console.log($("#popup").outerHeight());
+				// console.log($("#popup").outerWidth());
+
+				$("#popup").css("top", Math.max(0, (($(window).height() - $("#popup").outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+			    $("#popup").css("left", Math.max(0, (($(window).width() - $("#popup").outerWidth()) / 2) + 
+			                                                $(window).scrollLeft()) + "px");
+
+			    $("#popup img").css("width", $(window).width()*0.75);
+
+				if ($("#popup img").width() > $(window).width()) {
+					
+					$("#popup img").css("width", $(window).width()*0.75);
+					if ($("#popup img").height() > $(window).height()) {
+						$("#popup img").css("height", $(window).height()*0.75);
+					}
+				}
+
+				
+				$("#popup").fadeIn("fast");
+				/*$("#fade").show();*/
+
+				// $('body').append('<div id="fade"></div>');
+			});
+
+			$("#fade").click(function() {
+				$("#popup, #fade").fadeOut("fast");
 			});
 		});
 	};
