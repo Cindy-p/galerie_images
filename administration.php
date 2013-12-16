e13e03d405d346e2d39a0e375d9522b458120945
Cindy Perat <cindy.perat@live.fr>
2013-12-16 14:47:06 +0100

    quand je dors !

--- administration.php
+++ administration.php
@@ -15,56 +15,57 @@ include("include/connexion.php");
 	}
 
 ?>
-
-<div id="listCategorie">
-	<ul>
-		<li><a href="#nouvelleCategorie">Nouvelle Catégorie</a></li>
+<div id="corps">
+	<div id="listCategorie">
+		<ul>
+			<li><a href="#nouvelleCategorie">Nouvelle Catégorie</a></li>
+			<?php 
+				$sql = "SELECT * FROM categorie WHERE idutilisateur = :idutilisateur ";
+				$stm = $pdo->prepare($sql);
+				$stm->execute(array(":idutilisateur" => $_SESSION['idutilisateur']));
+				while( $categorie = $stm->fetch(PDO::FETCH_ASSOC) ){
+					echo "<li class='curseur'><a href='#categorie-".$categorie["idcategorie"]."' class='categorie-".$categorie["idcategorie"]."'>".$categorie["nom"]."</a></li>";
+				}
+			?>
+		</ul>
+		<div id="nouvelleCategorie">
+			<label for="nomCategorie">Nom de la Catégorie</label>
+			<input type="text" name="nomCategorie" id="nomCategorie" class="text ui-widget-content ui-corner-all">		
+			<button id="confirmNomCategorie" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only">Valider</button>
+			<p class="validateTips"></p>
+		</div>
 		<?php 
-			$sql = "SELECT * FROM categorie WHERE idutilisateur = :idutilisateur ";
-			$stm = $pdo->prepare($sql);
-			$stm->execute(array(":idutilisateur" => $_SESSION['idutilisateur']));
-			while( $categorie = $stm->fetch(PDO::FETCH_ASSOC) ){
-				echo "<li class='curseur'><a href='#categorie-".$categorie["idcategorie"]."' class='categorie-".$categorie["idcategorie"]."'>".$categorie["nom"]."</a></li>";
+			// Récupération des catégories
+			$sql = "SELECT * FROM categorie WHERE idutilisateur = :idutilisateur";
+			$stmCategorie = $pdo->prepare($sql);
+			$stmCategorie->execute(array(":idutilisateur" => $_SESSION["idutilisateur"]));
+			while( $categorie = $stmCategorie->fetch(PDO::FETCH_ASSOC) ){
+				echo "
+				<div id='categorie-".$categorie["idcategorie"]."'>
+					<h2><span id='nomCategorie-".$categorie["idcategorie"]."'>".$categorie["nom"]."</span>
+						<img id='supprimerCategorie-".$categorie["idcategorie"]."' src='img/croix.png' class='supprimerCategorie right petite_image curseur'/>
+						<img id='editCategorie-".$categorie["idcategorie"]."' src='img/vert.png' class='editCategorie center petite_image curseur'/>
+					</h2>
+					<div id='formImage'></div>
+						<button class='nouvelleImage ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only'>Nouvelle Image</button>
+						<ul class='listImage'>
+					";
+						// Récupération des images
+						$sql = "SELECT idimage, nom FROM image WHERE idcategorie = :idcategorie";
+						$stmImage = $pdo->prepare($sql);
+						$stmImage->execute(array(":idcategorie" => $categorie["idcategorie"]));
+						while( $image = $stmImage->fetch(PDO::FETCH_ASSOC) ){
+		  					echo "<li id='idImage-".$image["idimage"]."' class='ui-state-default'>
+		  							<span class='ui-icon ui-icon-arrowthick-2-n-s'></span><span class='editImage'>".$image["nom"]."</span>
+		  							<img id='supprimerImage-".$image["idimage"]."' src='img/croix.png' class='supprimerImage right petite_image curseur'/>
+		  						</li>";
+						}
+	  				echo "</ul>
+				</div>
+				" ;
 			}
 		?>
-	</ul>
-	<div id="nouvelleCategorie">
-		<label for="nomCategorie">Nom de la Catégorie</label>
-		<input type="text" name="nomCategorie" id="nomCategorie" class="text ui-widget-content ui-corner-all">		
-		<button id="confirmNomCategorie" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only">Valider</button>
-		<p class="validateTips"></p>
 	</div>
-	<?php 
-		// Récupération des catégories
-		$sql = "SELECT * FROM categorie WHERE idutilisateur = :idutilisateur";
-		$stmCategorie = $pdo->prepare($sql);
-		$stmCategorie->execute(array(":idutilisateur" => $_SESSION["idutilisateur"]));
-		while( $categorie = $stmCategorie->fetch(PDO::FETCH_ASSOC) ){
-			echo "
-			<div id='categorie-".$categorie["idcategorie"]."'>
-				<h2><span id='nomCategorie-".$categorie["idcategorie"]."'>".$categorie["nom"]."</span>
-					<img id='supprimerCategorie-".$categorie["idcategorie"]."' src='img/croix.png' class='supprimerCategorie right petite_image curseur'/>
-					<img id='editCategorie-".$categorie["idcategorie"]."' src='img/vert.png' class='editCategorie center petite_image curseur'/>
-				</h2>
-				<div id='formImage'></div>
-					<button class='nouvelleImage ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only'>Nouvelle Image</button>
-					<ul class='listImage'>
-				";
-					// Récupération des images
-					$sql = "SELECT idimage, nom FROM image WHERE idcategorie = :idcategorie";
-					$stmImage = $pdo->prepare($sql);
-					$stmImage->execute(array(":idcategorie" => $categorie["idcategorie"]));
-					while( $image = $stmImage->fetch(PDO::FETCH_ASSOC) ){
-	  					echo "<li id='idImage-".$image["idimage"]."' class='ui-state-default'>
-	  							<span class='ui-icon ui-icon-arrowthick-2-n-s'></span><span class='editImage'>".$image["nom"]."</span>
-	  							<img id='supprimerImage-".$image["idimage"]."' src='img/croix.png' class='supprimerImage right petite_image curseur'/>
-	  						</li>";
-					}
-  				echo "</ul>
-			</div>
-			" ;
-		}
-	?>
 </div>
 
 
