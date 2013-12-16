599a4578a4345478f2ffa46f872676e458408c88
VARNIER Nicolas <nicolablonde@hotmail.fr>
2013-12-16 12:31:48 +0100

    modif requete

--- index.php
+++ index.php
@@ -1,65 +1,6 @@
 <?php 
     $page = array( "titre" => "Nicody Galerie - Accueil");
-    include("include/header.php");
-    include("include/connexion.php");
-    include("include/fonction.php");
-
- ?>
-		<div id="corps">
-			<div id="galerie">
-			<?php
-			
-				// Création des catégories
-				$sql = "SELECT * FROM categorie WHERE idutilisateur = :idutilisateur ";
-				$stm = $pdo->prepare($sql);
-				$stm->execute(array(":idutilisateur" => $_SESSION['idutilisateur']));
-				
-				if( $stm->rowCount() == 0 ){
-					
-					echo "METS LE MESSAGE QUI TE PLAIT !";
-					
-				} else {
-
-					// Création de la liste de catégorie
-					echo "<ul>";
-					while( $categorie = $stm->fetch(PDO::FETCH_ASSOC) ){
-						echo "<li>".$categorie["nom"]."</li>";
-					}
-					echo "</ul><div>";
-					
-					// Création des catégories 
-					$sql = "SELECT * FROM categorie WHERE idutilisateur = :idutilisateur";
-					$stmCategorie = $pdo->prepare($sql);
-					$stmCategorie->execute(array(":idutilisateur" => $_SESSION["idutilisateur"]));
-					while( $categorie = $stmCategorie->fetch(PDO::FETCH_ASSOC) ){
-						echo "<div class='".strtolower(format_dossier($categorie['nom']))."'>";
-
-						// Récupération des images
-						$sql = "SELECT * FROM image WHERE idcategorie = :idcategorie";
-						$stmImage = $pdo->prepare($sql);
-						$stmImage->execute(array(":idcategorie" => $categorie["idcategorie"]));
-						while( $image = $stmImage->fetch(PDO::FETCH_ASSOC) ){
-							echo "	<div>
-										<img src='utilisateurs/".$_SESSION['utilisateur']."/".format_dossier($categorie['nom'])."/".$image['lien']."' alt=''/>
-										<div>
-											<span>".$image['nom']."</span>
-											<p>".$image['description']."</p>
-											<ul>
-												<li>sapin</li>
-												<li>neige</li>
-												<li>forêt</li>
-											</ul>
-										</div>
-									</div>";
-						}
-						echo "</div>";
-					}
-					echo "</div>";
-				}
-									
-			?>		
-			</div>
-		</div>
-<?php
-	 include ("include/footer.php");
+    include("include/header.php"); // Ajout du header 
+    include("corps.php"); // Ajout du corps de la page
+	include ("include/footer.php"); // Ajout du footer
 ?>
--- js/galerie.js
+++ js/galerie.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
 	// Réalise le "formatage" de la page 
 	$("#galerie").genererGalerie();
 	
+	// Autocompletion et recherche
+	$("#recherche").on("keyup",function(){
+		console.log($(this).val());
+		
+	});
+	
 });
